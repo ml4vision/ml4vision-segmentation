73821b2926fdcfae59e2dffb51191498e0d642d8
Davy Neven <neven.davy@gmail.com>
2022-10-07 14:41:23 +0200

    added train

---
+++ train.py
@@ -0,0 +1,24 @@
+from engine import Engine
+from configs.config import get_config
+from ml4vision.client import Client
+
+API_KEY = 'API_KEY'
+PROJECT_NAME = 'PROJECT_NAME'
+PROJECT_OWNER = None
+
+# init client
+client = Client(API_KEY)
+
+# load project
+project = client.get_project_by_name(PROJECT_NAME, owner=PROJECT_OWNER)
+project_location = project.pull(location='./data')
+
+# get config
+config = get_config(project_location=project_location, categories=project.categories)
+
+# train model
+engine = Engine(config)
+engine.train()
+
+# upload model
+engine.upload(project)
